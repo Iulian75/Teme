d595c8d6ff8a18005f30c9ffcb43f216f37aedfd
Iulian <robuiulyan@gmail.com>
2024-02-20 14:30:43 +0200

    tema7

---
+++ DiscordHomeWork/Homework7/Homework7.java
@@ -0,0 +1,42 @@
+package Teme.DiscordHomeWork.Homework7;
+
+public class Homework7 {
+    public static void main(String args[]){
+
+        //Exercitiu 1
+
+        int x = 5;
+        if (x> 10){
+            System.out.println("x is greater than 10");
+        } else {
+            System.out.println("x is not greater than 10");
+        }
+
+        //Exercitiu 2
+
+        String msj = "Hello";
+        if (msj.length() > 5){
+            System.out.println("The length of string is greater than 5");
+        } else {
+            if (msj.equals("Hello")){
+                System.out.println("The string is equal to Hello");
+            }else {
+                System.out.println("The string is neither grater than 5 nor equal to Hello");
+            }
+
+            //Exercitiu 3
+
+            int z= 20;
+            if (z >= 0 && z <= 50){
+                System.out.println("z falls into range of 0-50");
+            } else if (z >= 51 && z <= 100) {
+                System.out.println("z falls into range of 51-100");
+            } else if (z >= 101 && z <=150) {
+                System.out.println("z falls into range of 101-150");
+            } else {
+                System.out.println("z is outside range of 0-150");
+            }
+
+        }
+    }
+}
