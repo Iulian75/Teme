4e332ce196b684a21977dbfbf2acc3f4bf20909a
Iulian <robuiulyan@gmail.com>
2024-02-25 21:57:31 +0200

    folder 9 discord

---
+++ DiscordHomeWork/Homework9/Homework9.java
@@ -0,0 +1,61 @@
+package Teme.DiscordHomeWork.Homework9;
+
+public class Homework9 {
+
+    public static void main(String args[]){
+
+        //Exercitiu 1
+
+//        String str = "Hello";
+//
+//        if (str.length() > 5) {
+//            System.out.println("The length greater than 5");
+//        } else {
+//            if (str.equals("Hello")) {
+//                System.out.println("The string is equal to Hello.");
+//            } else {
+//                System.out.println("The string is not greater than 5 nor equal to Hello");
+//            }
+//        }
+
+        //Exercitiu 2
+
+//        char grade = 'P';
+//
+//        switch (grade) {
+//            case 'A':
+//                System.out.println("Excelent");
+//                break;
+//            case 'B':
+//                System.out.println("Good");
+//                break;
+//            case 'C':
+//                System.out.println("Average");
+//                break;
+//            default:
+//                System.out.println("You need to learn more");
+//        }
+
+
+        //Exercitiu 3
+
+//        String season= "Summer";
+//
+//        switch (season) {
+//            case "Winter":
+//                System.out.println("It's cold");
+//                break;
+//            case "Spring":
+//                System.out.println("Flowers bloom");
+//                break;
+//            case "Summer":
+//                System.out.println("It's hot");
+//                break;
+//            case "Autumn":
+//                System.out.println("Leaves fall");
+//                break;
+//            default:
+//                System.out.println("Check calendar again");
+//        }
+    }
+}
