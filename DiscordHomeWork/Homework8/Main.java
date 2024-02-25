d31a29aa177472ea199ed67b412ad1afb478357b
Iulian <robuiulyan@gmail.com>
2024-02-25 21:08:44 +0200

    tema 8 discord

---
+++ DiscordHomeWork/Homework8/Main.java
@@ -0,0 +1,61 @@
+package Teme.DiscordHomeWork.Homework8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        //Exercitiu 1
+
+//        Scanner scanner= new Scanner (System.in);
+//        System.out.println("Enter a number: ");
+//        int number = scanner.nextInt();
+//
+//        if (number > 0){
+//            System.out.println("The number is pozitive.");
+//        } else if (number < 0) {
+//            System.out.println("The number is negative: ");
+//        } else {
+//            System.out.println("The number is zero");
+//        }
+
+        //Exercitiu 2
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int number = scanner.nextInt();
+//
+//        if (number > 0) {
+//            System.out.println("The number is pozitive");
+//        } else {
+//            System.out.println("The number is negative");
+//        }
+
+
+        //Exercitiu 3
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter first number:");
+//        int number1 = scanner.nextInt();
+//
+//        System.out.println("Enter second number");
+//        int number2 = scanner.nextInt();
+//
+//        System.out.println("Enter third number");
+//        int number3 = scanner.nextInt();
+//
+//        int largestNumber = number1;
+//
+//        if (number2 > largestNumber){
+//            largestNumber = number2;
+//        }
+//
+//        if (number3 > largestNumber){
+//            largestNumber = number3;
+//        }
+//
+//        System.out.println("The largest number from: " + number1 + "/" + number2 + "/" + number3 + "is:" +largestNumber);
+
+
+    }
+}
