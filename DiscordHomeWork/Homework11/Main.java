f19f31cf654d1a81557620c1507176d4d048bc59
Iulian <robuiulyan@gmail.com>
2024-03-03 22:31:17 +0200

    tema 11

---
+++ DiscordHomeWork/Homework11/Main.java
@@ -0,0 +1,73 @@
+package Teme.DiscordHomeWork.Homework11;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+//        int day = 3;
+//
+//        switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Vine vara");
+//        }
+
+        //Exercitiu 2
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Numar: ");
+//        int n = scanner.nextInt();
+//
+//        int sum = 0;
+//        int i = 1;
+//
+//        while (i <= n) {
+//            sum += i;
+//            i++;
+//        }
+//
+//        System.out.println("suma = 1 to " + n + " is: " + sum);
+
+        //Exercitiu 3
+
+//        int number = 10;
+//
+//        do {
+//            System.out.println(number);
+//            number--;
+//        } while (number >= 1);
+//}
+
+        //Exercitiu 4
+        // Use a for loop to iterate over even numbers from 2 to 20
+
+        for (int number = 2; number <= 20; number += 2) {
+            System.out.println(number);
+
+        }
+    }
+}
+
+
